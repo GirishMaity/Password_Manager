11dee02403e7249556a425ab36872f08828eb571
Girish Maity <girishmaity164@gmail.com>
2022-08-28 19:00:22 +0530

    Resolved mongoose error

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,18 +1,14 @@
 const mongoose = require("mongoose");
 const DB = process.env.DATABASE;
 
-
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
-})
-.then(() =>
-{
+  })
+  .then(() => {
     console.log("Connection success.");
-})
-.catch((error) =>
-{
-    console.log(error)
-})
+  })
+  .catch((error) => {
+    console.log(error);
+  });
